6290004883a8d6a726add040da6793b290f7bca5
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2024-09-11 21:09:09 +0100

    generating comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #get "/articles", to: "articles#index"
   #get "/articles/:id", to: "articles#show"
-  # the above can be simplified as:
-  resources :articles
+  # the above can be simplified as L8:
+  resources :articles do
+    resources :comments
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
