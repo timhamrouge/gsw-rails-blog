91599c0ce71eb24287cb041f24309c825cc2238a
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2024-09-12 15:04:41 +0100

    guide finito

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "timo", password: "pw", only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
